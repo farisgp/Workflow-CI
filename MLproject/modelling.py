be9bbb7c7da97f1ba4fb9a9218ef1bbf5648bcd6
farisgp <112836845+farisgp@users.noreply.github.com>
2025-05-31 19:19:48 +0700

    Update modelling.py

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -46,7 +46,7 @@ X_scaled = scaler.fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
 input_example = X_train[0:5]
-time.sleep(10)
+
 with mlflow.start_run():
     mlflow.autolog()
     # Log parameters

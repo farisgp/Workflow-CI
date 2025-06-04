8f77be5d716a7d892be5a60fef0645286c944644
farisgp <farisghina51@gmail.com>
2025-06-04 14:15:20 +0700

    updated

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -1,8 +1,8 @@
 import mlflow
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import  mean_squared_error, r2_score
 import random
 import numpy as np
 import os
@@ -21,47 +21,44 @@ print("Tracking URI:", mlflow.get_tracking_uri())
 # print("Token Value:", os.environ.get("DAGSHUB_TOKEN")[:5], "...(disembunyikan)")  # For debugging, can be commented out
 
 # Create a new MLflow Experiment
-mlflow.set_experiment("Clothes Price Prediction")
+mlflow.set_experiment("CI Clothes Price")
 
 # Argument parser
 parser = argparse.ArgumentParser()
-parser.add_argument("--data_path", type=str, default="clothes_price_prediction_preprocessing.csv")
+parser.add_argument("--n_estimators", type=int, default=100)
+parser.add_argument("--max_depth", type=int, default=10)
 args = parser.parse_args()
 
-df = pd.read_csv(args.data_path)
+n_estimators = args.n_estimators
+max_depth = args.max_depth
 
-# Encode fitur kategorikal
-label_encoders = {}
-for column in df.select_dtypes(include=['object']).columns:
-    le = LabelEncoder()
-    df[column] = le.fit_transform(df[column])
-
-X = df.drop("Price", axis=1)
-y = df["Price"]
-
-# Normalisasi Fitur
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
-
-X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
-
-input_example = X_train[0:5]
+# --- Load Preprocessed Data
+X_train = pd.read_csv("X_train.csv")
+X_test = pd.read_csv("X_test.csv")
+y_train = pd.read_csv("y_train.csv").values.ravel()  # pastikan jadi 1D
+y_test = pd.read_csv("y_test.csv").values.ravel()  # pastikan jadi 1D
 
 with mlflow.start_run() as run:
-    mlflow.autolog()
-    # Log parameters
-    n_estimators = 505
-    max_depth = 37
-    # Train model
-    model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
-    model.fit(X_train, y_train)
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model",
-        input_example=input_example
-    )    # Log metrics
-    accuracy = model.score(X_test, y_test)
-    mlflow.log_metric("accuracy", accuracy)
+    model = RandomForestRegressor(
+        n_estimators=args.n_estimators,
+        max_depth=args.max_depth,
+        random_state=42
+    )
+    model.fit(X_train, y_train.values.ravel())
+
+    y_pred = model.predict(X_test)
+
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    # Logging parameter dan metrik secara manual
+    mlflow.log_param("n_estimators", args.n_estimators)
+    mlflow.log_param("max_depth", args.max_depth)
+    mlflow.log_metric("MSE", mse)
+    mlflow.log_metric("R2", r2)
+
+    # Log model
+    mlflow.sklearn.log_model(model, artifact_path="model", input_example=X_train.head())
 
     # Cetak run_id agar bisa digunakan di GitHub Actions
     run_id = run.info.run_id

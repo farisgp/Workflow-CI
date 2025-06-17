8bdf7f7d14148c0e3e80ee085be46933a1747139
farisgp <112836845+farisgp@users.noreply.github.com>
2025-06-17 15:28:41 +0700

    Update modelling.py

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import  mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 import random
 import numpy as np
 import joblib
@@ -39,26 +40,28 @@ y = df["Price"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 with mlflow.start_run() as run:
+    mlflow.log_param("n_estimators", 100)
+    mlflow.log_param("max_depth", 10)
+
     model = RandomForestRegressor(
         n_estimators=100,
         max_depth=10,
         random_state=42
     )
-    model.fit(X_train, y_train.values.ravel())
-
+    model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
 
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average='weighted', zero_division=0)
+    recall = recall_score(y_test, y_pred, average='weighted', zero_division=0)
 
-    # Logging parameter dan metrik secara manual
-    mlflow.log_param("n_estimators", 100)
-    mlflow.log_param("max_depth", 10)
-    mlflow.log_metric("MSE", mse)
-    mlflow.log_metric("R2", r2)
+    # Manual logging
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_metric("precision", precision)
+    mlflow.log_metric("recall", recall)
 
     # Log model
-    mlflow.sklearn.log_model(model, artifact_path="model")
+    mlflow.sklearn.log_model(model, artifact_path="model", input_example=X_train.head())
 
     # Cetak run_id agar bisa digunakan di GitHub Actions
     run_id = run.info.run_id

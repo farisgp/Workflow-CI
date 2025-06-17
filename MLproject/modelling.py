01fc0a62f3499322d3fd7cc4f5c031c25516d85a
farisgp <112836845+farisgp@users.noreply.github.com>
2025-06-17 16:31:50 +0700

    Update modelling.py

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -41,6 +41,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 with mlflow.start_run() as run:
     model = RandomForestRegressor(n_estimators=100, max_depth=10, random_state=42)
+    # Log model
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model",
+        input_example=X_train.head()
+    )
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
@@ -49,13 +55,6 @@ with mlflow.start_run() as run:
 
     mlflow.log_metric("MSE", mse)
     mlflow.log_metric("R2", r2)
-
-    # Log model
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model",
-        input_example=X_train.head()
-    )
 
 
     # Cetak run_id agar bisa digunakan di GitHub Actions

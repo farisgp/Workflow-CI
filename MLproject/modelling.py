7d95bb004ee17327ae9adbedff9c896096058796
farisgp <112836845+farisgp@users.noreply.github.com>
2025-06-17 18:17:15 +0700

    Update modelling.py

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -41,13 +41,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 with mlflow.start_run() as run:
     model = RandomForestRegressor(n_estimators=100, max_depth=10, random_state=42)
+    model.fit(X_train, y_train)
     # Log model
     mlflow.sklearn.log_model(
         sk_model=model,
         artifact_path="model",
         input_example=X_train.head()
     )
-    model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
     mse = mean_squared_error(y_test, y_pred)
